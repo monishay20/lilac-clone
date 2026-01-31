2a24a45beec634f9e5476962b0e9e87fafd0246a
Monisha <ymonisha284@gmail.com>
2026-02-01 00:24:32 +0530

    change font

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,43 @@
 import "./globals.css";
 import Header from "./components/Header";
-import { Poppins } from "next/font/google";
+// import { Poppins } from "next/font/google";
 
-const poppins = Poppins({
+// const poppins = Poppins({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600", "700"],
+// });
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={`${poppins.className} bg-white text-gray-900`}>
+//         <Header />
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-inter",
 });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${poppins.className} bg-white text-gray-900`}>
-        <Header />
-        {children}
+    <html lang="en" className={inter.variable}>
+      <body className="font-sans">{children}
+      <Header />
       </body>
+
     </html>
   );
 }
+
 
 
 
